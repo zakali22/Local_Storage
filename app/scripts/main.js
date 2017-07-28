015deae0e46cc9deb0de58b2adcae995434322fd
Zakeria Ali <zak.fined@gmail.com>
2017-07-28 22:35:14 +0100

    Add style and functionality to the list items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var list = JSON.parse(localStorage.getItem('items')) || [];
+  var itemsList = $('.menu-list');
+
+  populate(list, itemsList);
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    var text = $('form input').val();
+    var items = {
+      text,
+      done: false
+    };
+
+    list.push(items);
+    populate(list, itemsList);
+    localStorage.setItem('items', JSON.stringify(list));
+    console.table(list);
+    this.reset();
+  });
+
+  function populate(array = [], locationList) {
+    var list = array.map(function(plate, index) {
+      var output = '<li>';
+      output += '<input type="checkbox" data-index=item' + index + '" id="item' + index + '">';
+      output += '<label for="item' + index + '"><p>' + plate.text + '</p></label>';
+      output += '</li>';
+      return output;
+    }).join('');
+    itemsList.html(list);
+    console.log(list);
+  }
+});
